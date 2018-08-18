11d5c8f20b58d64d07e45e9f49049496d713980a
Allen Kan <akanch95@gmail.com>
2018-08-17 22:55:47 -0700

    successful token tests shown on gameboard

--- token.js
+++ token.js
@@ -6,7 +6,15 @@ function Token(color) {
 // blue token slots
 Token.prototype.draw = function(x, y) {
   ctx.beginPath();
-  ctx.fillStyle(this.color)
-  ctx.arc(x, y, radiusBlue, 0,2*Math.PI);
-  ctx.stroke();
+  ctx.fillStyle = this.color;
+  if (this.color == '#d50000') {
+    ctx.arc(x, y, radiusRed, 0,2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  }
+  else {
+    ctx.arc(x, y, radiusBlue, 0,2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  };
 };
