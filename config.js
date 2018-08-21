b519453184a55d2e48d0b584242013a88b9fb3d2
Allen Kan <akanch95@gmail.com>
2018-08-21 12:26:23 -0700

    main function edited

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 // function to storing the current configuration of the game
-function config(currentDeck, numBlueTokens, numRedTokens, currentPlayer, numPlayers, discards, correctGuesses, wrongGuesses) { //config should be in its own file {
+function config(currentDeck, numBlueTokens, currentPlayer, numPlayer, discards, correctGuesses, wrongGuesses) { //config should be in its own file {
 	this.currentDeck = currentDeck;
 	this.numBlueTokens = numBlueTokens;
-	this.numRedTokens = numRedTokens;
   this.currentPlayer = currentPlayer;
-  this.numPlayers = numPlayers;
+  this.numPlayer = numPlayer;
   this.discards = discards;
   this.correctGuesses = correctGuesses;
   this.wrongGuesses = wrongGuesses;
--- main.js
+++ main.js
@@ -1,65 +1,3 @@
-// currently testing in game.js
-/*
-function draw() {
-  // creating the board for the game
-  ctx.rect(0, 0, windowX, windowY);
-  ctx.fillStyle = '#6e777a';
-  ctx.fill();
-
-  // slot for the deck
-  ctx.rect(deckX, deckY, cardWidth, cardHeight);
-  ctx.stroke();
-
-  // slot for each color of discarded cards
-  var deckX = 15;
-  var yDiscard = 275;
-  for (i = 0; i < 4; i++) {
-    ctx.rect(deckX, yDiscard, cardWidth, cardHeight);
-    ctx.stroke();
-    yDiscard += cardHeight + 5;
-  };
-
-  // blue token slots
-  var xFirstRow = 35;
-  var xSecondRow = 35;
-  var radiusBlue = 20;
-  var yFirstRow = cardHeight + 55
-  var ySecondRow = yFirstRow + + radiusBlue * 2 + 10;
-  for (i = 0; i < 8; i++) {
-    if (i < 4) {
-      ctx.beginPath();
-      ctx.arc(xFirstRow, yFirstRow, radiusBlue, 0,2*Math.PI);
-      ctx.stroke();
-
-      // first row blue token drawing test
-      blue = new Token('#02a8ff');
-      blue.draw(xFirstRow, yFirstRow);
-      xFirstRow += 50;
-    }
-    else{
-      ctx.beginPath();
-      ctx.arc(xSecondRow, ySecondRow, radiusBlue, 0,2*Math.PI);
-      ctx.stroke();
-
-      // second row blue token drawing test
-      blue = new Token('#02a8ff');
-      blue.draw(xSecondRow, ySecondRow);
-      xSecondRow += 50;
-    }
-  };
-
-
-  // shuffled deck created
-  var newDeck = new Deck([1, 1, 1, 2, 2, 3, 3, 4, 4, 5]);
-  deckX = 15;
-  var deckY = 15;
-  newDeck.shuffle();
-  for (i = 0; i < newDeck.cards.length; i++){
-    newDeck.cards[i].draw(deckX, deckY);
-    deckX += 10;
-  };
-  */
-
   // discard piles
   blueDiscard = new Discard();
   greenDiscard = new Discard();
@@ -121,7 +59,7 @@ function Main() {
   deck.shuffle();
   var playerList = [];
   dealcards[playerList];
-  var config(0, 0, deck, players);
+  var config = new config(deck, 8, playerList[0], playerList.length(), [], 0, 0);
   if startGame == True {
     runGame(config)
   };
